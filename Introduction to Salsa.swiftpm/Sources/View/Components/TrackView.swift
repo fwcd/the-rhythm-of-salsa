64c9e30bdce5b9565be65b106807fe1cd3872755
fwcd <fwcd@fwcd.dev>
2024-02-11 19:36:14 +0100

    Add support for sub-beats in TrackView

--- Introduction to Salsa.swiftpm/Sources/View/Components/TrackView.swift
+++ Introduction to Salsa.swiftpm/Sources/View/Components/TrackView.swift
@@ -4,6 +4,15 @@ struct TrackView: View {
     @Binding var track: Track
     @Binding var playhead: Beats
     let beatSize: CGFloat
+    let squaresPerBeat: Int = 2
+    
+    private var beatCount: Int {
+        Int(track.length.rawValue.rounded(.up))
+    }
+            
+    private var squareCount: Int {
+        beatCount * squaresPerBeat
+    }
     
     private var loopedPlayhead: Beats {
         track.isLooping
@@ -19,8 +28,8 @@ struct TrackView: View {
                 .frame(width: imageSize, height: imageSize)
                 .padding((beatSize - imageSize) / 2)
                 .foregroundStyle(track.instrument.color)
-            ForEach(0..<Int(track.length.rawValue.rounded(.up)), id: \.self) { i in
-                let beatRange = Beats(i)..<Beats(i + 1)
+            ForEach(0..<squareCount, id: \.self) { i in
+                let beatRange = (Beats(i) / Beats(squaresPerBeat))..<(Beats(i + 1) / Beats(squaresPerBeat))
                 let shape = RoundedRectangle(cornerRadius: ViewConstants.cornerRadius)
                 let color = track.instrument.color
                 shape
@@ -38,7 +47,7 @@ struct TrackView: View {
                     .frame(width: beatSize, height: beatSize)
                     .onTapGesture {
                         if track.findEvents(in: beatRange).isEmpty {
-                            track.replaceEvents(in: beatRange, with: Event())
+                            track.replaceEvents(in: beatRange, with: Event(duration: 1 / Beats(squaresPerBeat)))
                         } else {
                             track.removeEvents(in: beatRange)
                         }
