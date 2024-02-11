aff511c567483f774cdfd5c4a366a6364d3219d6
fwcd <fwcd@fwcd.dev>
2024-02-11 03:16:48 +0100

    Add some more Beats conformances

--- Introduction to Salsa.swiftpm/Sources/Model/Beats.swift
+++ Introduction to Salsa.swiftpm/Sources/Model/Beats.swift
@@ -2,6 +2,34 @@ struct Beats: Codable, Hashable, RawRepresentable {
     var rawValue: Double
 }
 
+extension Beats: AdditiveArithmetic {
+    static let zero = Beats(rawValue: 0)
+    
+    static func +(lhs: Beats, rhs: Beats) -> Beats {
+        Beats(rawValue: lhs.rawValue + rhs.rawValue)
+    }
+    
+    static func -(lhs: Beats, rhs: Beats) -> Beats {
+        Beats(rawValue: lhs.rawValue - rhs.rawValue)
+    }
+}
+
+extension Beats: Comparable {
+    static func <(lhs: Beats, rhs: Beats) -> Bool {
+        lhs.rawValue < rhs.rawValue
+    }
+}
+
+extension Beats: Strideable {
+    func distance(to other: Beats) -> Double {
+        other.rawValue - rawValue
+    }
+    
+    func advanced(by n: Double) -> Beats {
+        Beats(rawValue: rawValue + n)
+    }
+}
+
 extension Beats: ExpressibleByIntegerLiteral {
     init(integerLiteral value: Int) {
         self.init(rawValue: Double(value))
