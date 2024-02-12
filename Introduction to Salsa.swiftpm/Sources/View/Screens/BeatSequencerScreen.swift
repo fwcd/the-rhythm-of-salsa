e4cf8024cc7197618285a0b8030747e8ff9ebd87
fwcd <fwcd@fwcd.dev>
2024-02-12 05:03:51 +0100

    Experimentally try loading the piano riff

--- Introduction to Salsa.swiftpm/Sources/View/Screens/BeatSequencerScreen.swift
+++ Introduction to Salsa.swiftpm/Sources/View/Screens/BeatSequencerScreen.swift
@@ -7,6 +7,11 @@ struct BeatSequencerScreen: View {
         SharedBeatSequencerView()
             .onAppear {
                 engine.model.tracks = Instrument.allCases.map { Track(preset: .init(instrument: $0)) }
+                
+                // FIXME: Figure out where to actually load the MIDI riff
+                let midiURL = Bundle.main.url(forResource: "Riff I-I-V-V", withExtension: "mid")!
+                let midi = try! BeatSequencerModel(midiFileURL: midiURL)
+                engine.model.tracks.append(midi.tracks.first!)
             }
             .navigationTitle("Beat Sequencer")
     }
