935fe982c3f1bafed19a88b9f3cfb0e95d824755
fwcd <fwcd@fwcd.dev>
2024-02-16 01:01:49 +0100

    Play bongos harder

--- Introduction to Salsa.swiftpm/Sources/View/Extensions/Instrument+Patterns.swift
+++ Introduction to Salsa.swiftpm/Sources/View/Extensions/Instrument+Patterns.swift
@@ -4,7 +4,7 @@ extension Instrument {
         case .clave: [events(for: [1, 2, 4, 5.5, 7])] // 2-3 pattern
         case .cowbell: [events(for: [0, 4])]
         case .congas: [events(for: [3, 3.5, 7, 7.5])]
-        case .bongos: [events(for: Array(0..<16).map { Beats($0) / 2 }) { $0 % 2 == 0 ? 0.4 : 0.2 }]
+        case .bongos: [events(for: Array(0..<16).map { Beats($0) / 2 }) { $0 % 2 == 0 ? 0.8 : 0.4 }]
         case .maracas: [events(for: [1, 1.5, 2, 2.5, 3.5, 4, 4.5, 5, 6, 6.5, 7, 7.5])]
         case .timbales: [events(for: [0, 1, 2, 2.5, 3.5, 4, 5, 5.5, 6.5, 7])]
         default: [[]]
