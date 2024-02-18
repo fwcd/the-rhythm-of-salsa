e2894807ff63b4df0730b90d952d686edaec92f3
fwcd <fwcd@fwcd.dev>
2024-02-18 23:29:05 +0100

    Use slightly neater formatting

--- Introduction to Salsa.swiftpm/Sources/View/Extensions/Instrument+Tutorial.swift
+++ Introduction to Salsa.swiftpm/Sources/View/Extensions/Instrument+Tutorial.swift
@@ -1,18 +1,15 @@
 extension Instrument {
     var tutorialDescription: [String] {
         switch self {
-        case .clave:
-            [
-                #"The clave is one of the most fundamental instruments in Salsa music. Most commonly it plays a "2-3" pattern, i.e. 2 strokes on the first measure and 3 strokes on the second measure."#,
-                #"Notice how the clave does not play on the first beat. The "off-beaty" feeling is a cornerstone of the polyrhythmic Salsa beat."#,
-                "Now try editing the beat yourself by clicking the colored pads below.",
-            ]
-        case .cowbell:
-            [
-                "While the clave often plays on the off-beats, the cowbell is generally on the beat. This provides a rhythmic framework that makes it easier to identify the beats by ear."
-            ]
-        default:
-            []
+        case .clave: [
+            #"The clave is one of the most fundamental instruments in Salsa music. Most commonly it plays a "2-3" pattern, i.e. 2 strokes on the first measure and 3 strokes on the second measure."#,
+            #"Notice how the clave does not play on the first beat. The "off-beaty" feeling is a cornerstone of the polyrhythmic Salsa beat."#,
+            "Now try editing the beat yourself by clicking the colored pads below.",
+        ]
+        case .cowbell: [
+            "While the clave often plays on the off-beats, the cowbell is generally on the beat. This provides a rhythmic framework that makes it easier to identify the beats by ear."
+        ]
+        default: []
         }
     }
 }
