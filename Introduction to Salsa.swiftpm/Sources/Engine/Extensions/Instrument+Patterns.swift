0a6a3b378539f982bffcb4ccf7ee7e0064f4b784
fwcd <fwcd@fwcd.dev>
2024-02-18 23:34:17 +0100

    Add basic cowbell rhythm

--- Introduction to Salsa.swiftpm/Sources/Engine/Extensions/Instrument+Patterns.swift
+++ Introduction to Salsa.swiftpm/Sources/Engine/Extensions/Instrument+Patterns.swift
@@ -7,7 +7,10 @@ extension Instrument {
             pattern("2-3 Pattern", beats: [1, 2, 4, 5.5, 7]),
             pattern("3-2 Pattern", beats: [0, 1.5, 3, 5, 6]),
         ]
-        case .cowbell: [pattern("Downbeats", beats: [0, 4])]
+        case .cowbell: [
+            pattern("Beats", beats: [0, 2, 4, 6]),
+            pattern("Downbeats", beats: [0, 4]),
+        ]
         case .congas: [pattern("Tumbao", beats: [3, 3.5, 7, 7.5])]
         case .bongos: [pattern(beats: Array(0..<16).map { Beats($0) / 2 }) { $0 % 2 == 0 ? 0.8 : 0.4 }]
         case .maracas: [pattern(beats: [1, 1.5, 2, 2.5, 3.5, 4, 4.5, 5, 6, 6.5, 7, 7.5])]
