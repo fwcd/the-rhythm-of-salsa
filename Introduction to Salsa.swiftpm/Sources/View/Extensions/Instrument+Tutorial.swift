a3e9b80607c943addad0b83b59ac5957ccb335f5
fwcd <fwcd@fwcd.dev>
2024-02-19 22:22:24 +0100

    Add tutorial descriptions for congas and bongos

--- Introduction to Salsa.swiftpm/Sources/View/Extensions/Instrument+Tutorial.swift
+++ Introduction to Salsa.swiftpm/Sources/View/Extensions/Instrument+Tutorial.swift
@@ -7,7 +7,13 @@ extension Instrument {
             "Now try editing the beat yourself by clicking the colored rectangular pads below. You can switch back to one of the predefined patterns using the drop-down menu on the right.",
         ]
         case .cowbell: [
-            "While the clave often plays on the off-beats, the cowbell is generally on the beat. This provides a rhythmic framework that makes it easier to identify the beats by ear."
+            "While the clave often plays on the off-beats, the cowbell is generally on the beat. This provides a rhythmic framework that makes it easier to identify the beats by ear.",
+        ]
+        case .congas: [
+            #"Congas are tall drums with a sonorous sound. Their rhythm, called "Tumbao", is in its simplest form characterized by playing the last two half-beats in each measure."#,
+        ]
+        case .bongos: [
+            "Bongos are pairs of smaller drums that play a steady pattern with a stroke on every half-beat.",
         ]
         default: []
         }
