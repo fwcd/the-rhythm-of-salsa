4e720a6a0ec5b85c57cc76a41067cfcf9c6b7402
fwcd <fwcd@fwcd.dev>
2024-02-19 06:30:01 +0100

    Set volumes in patterns

--- Introduction to Salsa.swiftpm/Sources/Engine/Extensions/Instrument+Patterns.swift
+++ Introduction to Salsa.swiftpm/Sources/Engine/Extensions/Instrument+Patterns.swift
@@ -8,10 +8,10 @@ extension Instrument {
             pattern("3-2 Pattern", beats: [0, 1.5, 3, 5, 6]),
         ]
         case .cowbell: [
-            pattern("Beats", beats: [0, 2, 4, 6]),
-            pattern("Downbeats", beats: [0, 4]),
+            pattern("Beats", volume: 0.3, beats: [0, 2, 4, 6]),
+            pattern("Downbeats", volume: 0.3, beats: [0, 4]),
         ]
-        case .congas: [pattern("Tumbao", beats: [3, 3.5, 7, 7.5])]
+        case .congas: [pattern("Tumbao", volume: 0.8, beats: [3, 3.5, 7, 7.5])]
         case .bongos: [pattern(beats: Array(0..<16).map { Beats($0) / 2 }) { $0 % 2 == 0 ? 0.8 : 0.4 }]
         case .maracas: [pattern(beats: [1, 1.5, 2, 2.5, 3.5, 4, 4.5, 5, 6, 6.5, 7, 7.5])]
         case .timbales: [pattern(beats: [0, 1, 2, 2.5, 3.5, 4, 5, 5.5, 6.5, 7])]
@@ -32,9 +32,10 @@ extension Instrument {
     }
 }
 
-private func pattern(_ name: String = "Default", beats: [Beats] = [], velocity: (Int) -> Double = { _ in 1 }) -> Pattern {
+private func pattern(_ name: String = "Default", volume: Double = 1, beats: [Beats] = [], velocity: (Int) -> Double = { _ in 1 }) -> Pattern {
     Pattern(
         name: name,
+        volume: volume,
         offsetEvents: beats.enumerated().map { (i, offset) in
             OffsetEvent(event: Event(velocity: UInt32(127 * velocity(i)), duration: 0.5), startOffset: offset)
         }
