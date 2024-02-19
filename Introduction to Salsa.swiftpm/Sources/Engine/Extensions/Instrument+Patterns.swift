0c6fa4cc20780f76b315d4cda889a996caaf9808
fwcd <fwcd@fwcd.dev>
2024-02-19 22:32:44 +0100

    Rename patterns

--- Introduction to Salsa.swiftpm/Sources/Engine/Extensions/Instrument+Patterns.swift
+++ Introduction to Salsa.swiftpm/Sources/Engine/Extensions/Instrument+Patterns.swift
@@ -11,10 +11,18 @@ extension Instrument {
             pattern("Beats", volume: 0.3, beats: [0, 2, 4, 6]),
             pattern("Downbeats", volume: 0.3, beats: [0, 4]),
         ]
-        case .congas: [pattern("Tumbao", volume: 0.8, beats: [3, 3.5, 7, 7.5])]
-        case .bongos: [pattern("Steady", beats: Array(0..<16).map { Beats($0) / 2 }) { $0 % 2 == 0 ? 0.8 : 0.4 }]
-        case .maracas: [pattern("Steady", beats: Array(0..<16).map { Beats($0) / 2 }) { $0 % 2 == 0 ? 0.8 : 0.4 }]
-        case .timbales: [pattern(beats: [0, 1, 2, 2.5, 3.5, 4, 5, 5.5, 6.5, 7])]
+        case .congas: [
+            pattern("Tumbao", volume: 0.8, beats: [3, 3.5, 7, 7.5]),
+        ]
+        case .bongos: [
+            pattern("Steady", beats: Array(0..<16).map { Beats($0) / 2 }) { $0 % 2 == 0 ? 0.8 : 0.4 },
+        ]
+        case .maracas: [
+            pattern("Steady", beats: Array(0..<16).map { Beats($0) / 2 }) { $0 % 2 == 0 ? 0.8 : 0.4 },
+        ]
+        case .timbales: [
+            pattern("Cascara", beats: [0, 1, 2, 2.5, 3.5, 4, 5, 5.5, 6.5, 7]),
+        ]
         case .piano:
             memo([self]) {
                 (Bundle.main.urls(forResourcesWithExtension: "mid", subdirectory: nil) ?? [])
