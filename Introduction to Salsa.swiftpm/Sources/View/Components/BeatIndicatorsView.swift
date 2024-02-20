720f7be3958d034a870d1f0cea94d016afdd911b
fwcd <fwcd@fwcd.dev>
2024-02-20 02:55:17 +0100

    Factor out loopedPlayhead

--- Introduction to Salsa.swiftpm/Sources/View/Components/BeatIndicatorsView.swift
+++ Introduction to Salsa.swiftpm/Sources/View/Components/BeatIndicatorsView.swift
@@ -5,12 +5,16 @@ struct BeatIndicatorsView: View {
     var padSize: CGFloat = 64
     var options: TrackOptions = .init()
     
+    private var loopedPlayhead: Beats {
+        playhead.truncatingRemainder(dividingBy: Beats(options.beatCount))
+    }
+    
     var body: some View {
         TrackRow(padSize: padSize, options: options) { position, beatRange in
             Group {
                 if position.padInBeat == 0 {
                     Text(String(1 + position.beatIndex))
-                        .scaleEffect(beatRange.contains(playhead.truncatingRemainder(dividingBy: Beats(options.beatCount))) ? 1.2 : 1)
+                        .scaleEffect(beatRange.contains(loopedPlayhead) ? 1.2 : 1)
                 } else {
                     Text("+")
                         .opacity(0.3)
