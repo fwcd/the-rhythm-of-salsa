a878e3564fc092cdc9b01af102bc013d969a2ffb
fwcd <fwcd@fwcd.dev>
2024-02-22 04:09:02 +0100

    Add experimental convenience method for creating MIDIMetaEvent

--- The Rhythm of Salsa.swiftpm/Sources/Engine/Extensions/MIDIMetaEvent+Data.swift
+++ The Rhythm of Salsa.swiftpm/Sources/Engine/Extensions/MIDIMetaEvent+Data.swift
@@ -1,9 +1,24 @@
 import AVFoundation
 
 extension MIDIMetaEvent {
-    var rawData: Data {
+    var raw: Data {
         withUnsafePointer(to: data) { dataPointer in
             Data(Array(UnsafeBufferPointer(start: dataPointer, count: Int(dataLength))))
+        }
+    }
+    
+    static func create(metaEventType: UInt8, raw: Data) -> Guard<UnsafeMutablePointer<MIDIMetaEvent>> {
+        let size = MemoryLayout<MIDIMetaEvent>.size + raw.count
+        let pointer = UnsafeMutablePointer<MIDIMetaEvent>.allocate(capacity: size)
+        
+        pointer.pointee.dataLength = UInt32(raw.count)
+        withUnsafeMutablePointer(to: &pointer.pointee.data) { dataPointer in
+            let bufferPointer = UnsafeMutableBufferPointer(start: dataPointer, count: raw.count)
+            raw.copyBytes(to: bufferPointer, count: raw.count)
+        }
+        
+        return Guard(wrappedValue: pointer) {
+            pointer.deallocate()
         }
     }
 }
