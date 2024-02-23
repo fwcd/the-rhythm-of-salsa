8afb79a09d9779770935d434a177fa64e4c0ea2e
fwcd <fwcd@fwcd.dev>
2024-02-23 01:41:48 +0100

    Use a darker yellow in light mode
    Fixes #12

--- The Rhythm of Salsa.swiftpm/Sources/View/Extensions/Instrument+Color.swift
+++ The Rhythm of Salsa.swiftpm/Sources/View/Extensions/Instrument+Color.swift
@@ -3,13 +3,22 @@ import SwiftUI
 extension Instrument {
     func color(for colorScheme: ColorScheme) -> Color {
         switch self {
-        case .clave: .orange
-        case .cowbell: .yellow
-        case .congas: .red
-        case .bongos: .purple
-        case .maracas: .blue
-        case .timbales: .green
-        case .piano: .primary
+        case .clave:
+            .orange
+        case .cowbell:
+            colorScheme == .dark
+                ? .yellow
+                : .init(hue: 0.14, saturation: 1, brightness: 0.85)
+        case .congas:
+            .red
+        case .bongos:
+            .purple
+        case .maracas:
+            .blue
+        case .timbales:
+            .green
+        case .piano:
+            .primary
         }
     }
 }
@@ -18,4 +27,14 @@ extension Color {
     init(_ instrument: Instrument, for colorScheme: ColorScheme) {
         self = instrument.color(for: colorScheme)
     }
+}
+
+#Preview {
+    BeatSequencerView(
+        model: .constant(.init(tracks: Instrument.allCases.map { instrument in
+            Track(instrument: instrument)
+        })),
+        playhead: .constant(0),
+        isPlaying: .constant(false)
+    )
 }
